985e5d1c3d878a3d42667ed74fd388ebeb856cd9
Krishna Agarwal <dmkrishna.agarwal@gmail.com>
2022-07-30 16:02:43 +0530

    Update getTopFollowers.py

--- src/getTopFollowers.py
+++ src/getTopFollowers.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
 
     html = "<table>\n"
 
-    for i in range(min(len(followers), 14)):
+    for i in range(min(len(followers), 20)):
         login = followers[i][1]
         id = followers[i][2]
         name = followers[i][3]
